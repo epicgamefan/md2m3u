1a0de5ba7ac3c5ca72de0b7d463459e715f70f7e
Joshua Luoni <josh.luoni@gmail.com>
2022-08-20 20:09:37 -0400

    Add project files.

---
+++ FileScanner.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace md2m3u
+{
+    internal class FileScanner
+    {
+
+        private string[] _allowedExtensions = new[] { ".chd", ".iso", ".cue" };
+        public string[] AllowedExtensions
+        {
+            get
+            {
+                return _allowedExtensions;
+            }
+            set
+            {
+                _allowedExtensions = value;
+            }
+        }
+
+        private string _searchFolder = ".";
+        public string SearchFolder
+        {
+            get
+            {
+                return _searchFolder;
+            }
+            set
+            {
+                _searchFolder = value;
+            }
+        }
+
+        private SearchOption _searchOption = SearchOption.TopDirectoryOnly;
+        public bool Recursive
+        {
+            get
+            {
+                return (_searchOption == SearchOption.AllDirectories);
+            }
+            set
+            {
+                if(value)
+                {
+                    _searchOption = SearchOption.AllDirectories;
+                }
+                else
+                {
+                    _searchOption = SearchOption.TopDirectoryOnly;
+                } 
+            }
+        }
+
+        public FileScanner()
+        {
+        }
+
+        internal List<string> Scan()
+        {
+            
+            List<string> discImages = Directory
+                .GetFiles(_searchFolder, "*", _searchOption)
+                .Where(file => _allowedExtensions.Any(file.ToLower().EndsWith))
+                .ToList();
+
+            return discImages;
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace md2m3u
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Scan Directory Recursively For chd/iso/cue Image Files
+            FileScanner scanner = new FileScanner();
+            scanner.SearchFolder = ".";
+            scanner.AllowedExtensions = new[] { ".chd", ".iso", ".cue" };
+            scanner.Recursive = true;
+            List<string> discImages = scanner.Scan();
+
+            // Turn Disc Images paths into Game objects.
+            List<Game> games = new List<Game>();
+            foreach(var di in discImages)
+            {
+                Game g = new Game(di);
+                games.Add(g);
+            }
+
+            // Combine multi disc games
+            List<Game> processedGames = Game.CombineGames(games);
+
+            // Write m3u files
+            foreach (var g in processedGames)
+            {
+                if(g.DiscCount > 1)
+                {
+                    File.WriteAllText(g.M3uFile, g.M3uContent);
+                }
+            }
+        }
+    }
+}
